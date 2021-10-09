f46cfd1336876aafa58c11fff083dbca8dcf6396
Bryann Sotomayor <bryann.sotomayor@gmail.com>
2021-10-08 22:14:47 -0700

    Create the CreateMemeImg component.

---
+++ src/components/CreateMemeImg.js
@@ -0,0 +1,11 @@
+import './CreateMemeImg.css';
+const CreateMemeImg = props => {
+  //props: imgIdx, imgData, texts, isAllCaps, fontIdx
+  return (
+    <div className='CreateMemeImg'>
+
+    </div>
+  );
+};
+
+export default CreateMemeImg;
