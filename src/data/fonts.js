9c08bb9dd64ac27abe66e19871ae7f90c4ab275b
Bryann Sotomayor <bryann.sotomayor@gmail.com>
2021-10-08 20:09:29 -0700

    Add the fonts.js data file.

---
+++ src/data/fonts.js
@@ -0,0 +1,11 @@
+const fontsData = [
+  { family: 'Roboto', weight: 900 },
+  { family: 'Open Sans', weight: 800 },
+  { family: 'Helvetica', weight: 'bolder' },
+  { family: 'Arial', weight: 'bolder' },
+  { family: 'Verdana', weight: 'bolder' },
+  { family: 'Archivo Black', weight: 400 },
+  { family: 'Impact', weight: 'bolder' }
+];
+
+export default fontsData;
