c1c8550e17a7436c47630ed6013de3ed2d06820f
Bryann Sotomayor <bryann.sotomayor@gmail.com>
2021-10-09 12:54:14 -0700

    Make the text in the CreateMemeText component change fonts when the user selects a different font.

--- src/components/CreateMemeText.js
+++ src/components/CreateMemeText.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CreateMemeText.css';
+import fontsData from '../data/fonts';
 export default class CreateMemeText extends React.Component {
   state = {
 
@@ -7,10 +8,13 @@ export default class CreateMemeText extends React.Component {
 
   render = () => {
     const { state, props } = this;
-    const { text, isAllCaps } = props;
+    const { text, isAllCaps, fontIdx } = props;
 
+    const font = fontsData[fontIdx];
     const style = {
-      textTransform: isAllCaps ? 'uppercase' : 'none'
+      textTransform: isAllCaps ? 'uppercase' : 'none',
+      fontFamily: font.family,
+      fontWeight: font.weight
     };
     return (<p className='CreateMemeText' style={style}>{text}</p>);
   };
