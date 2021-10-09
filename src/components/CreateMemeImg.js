acb5a2fa2b5d7705de4f351e759b6847cd6f159b
Bryann Sotomayor <bryann.sotomayor@gmail.com>
2021-10-09 10:24:33 -0700

    Display and style meme image as background image of the CreateMemeImg component.

--- src/components/CreateMemeImg.js
+++ src/components/CreateMemeImg.js
@@ -1,9 +1,16 @@
 import './CreateMemeImg.css';
-const CreateMemeImg = props => {
-  //props: imgIdx, imgData, texts, isAllCaps, fontIdx
-  return (
-    <div className='CreateMemeImg'>
+import CreateMemeText from './CreateMemeText';
 
+const CreateMemeImg = props => {
+  console.log('CreateMemeImg rendered');
+  const { imgsData, imgIdx, texts: { text1, text2 }, isAllCaps, fontIdx } = props;
+  const createMemeTextProps = { isAllCaps, fontIdx };
+
+  const { url } = imgsData[imgIdx];
+  const style = { backgroundImage: `url(${url})` };
+  return (
+    <div className='CreateMemeImg' style={style}>
+      <CreateMemeText {...createMemeTextProps} text={text1} />
     </div>
   );
 };
