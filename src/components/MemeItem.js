b9f8af21f0266914e06a989f9b8e57e1f85f5eb8
Bryann Sotomayor <bryann.sotomayor@gmail.com>
2021-11-18 20:48:59 -0800

    Refactor the MemeItem componenent to become a functional component.

--- src/components/MemeItem.js
+++ src/components/MemeItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
@@ -9,20 +9,20 @@ import EditMemeForm from './EditMemeForm';
 import MemeImg from './MemeImg';
 import EditMemeImg from './EditMemeImg';
 
-export default class MemeItem extends React.Component {
-  state = {
+const MemeItem = props => {
+  const [memeData, setMemeDataTo] = useState({
     editModeOn: false,
-    imgIdx: this.props.imgIdx,
-    texts: this.props.texts,
-    isAllCaps: this.props.isAllCaps,
-    fontIdx: this.props.fontIdx,
-    fontSize: this.props.fontSize,
-    pos: this.props.pos
-  };
+    imgIdx: props.imgIdx,
+    texts: props.texts,
+    isAllCaps: props.isAllCaps,
+    fontIdx: props.fontIdx,
+    fontSize: props.fontSize,
+    pos: props.pos
+  });
 
-  resetState = () => {
-    const { imgIdx, texts, isAllCaps, fontIdx, fontSize, pos } = this.props;
-    this.setState({
+  const resetState = () => {
+    const { imgIdx, texts, isAllCaps, fontIdx, fontSize, pos } = props;
+    setMemeDataTo({
       editModeOn: false,
       imgIdx,
       texts,
@@ -33,65 +33,79 @@ export default class MemeItem extends React.Component {
     });
   };
 
-  setEditModeTo = value => this.setState({ editModeOn: value });
+  const setEditModeTo = value => setMemeDataTo(memeData => ({
+    ...memeData,
+    editModeOn: value
+  }));
 
-  setTexts = ({ currentTarget: { name, value } }) =>
-    this.setState(state =>
-      ({ 'texts': { ...state.texts, [name]: value } }));
+  const setTexts = ({ currentTarget: { name, value } }) => setMemeDataTo(memeData => ({
+    ...memeData,
+    'texts': {
+      ...memeData.texts,
+      [name]: value
+    }
+  }));
 
-  toggleAllCaps = () => this.setState(state =>
-    ({ isAllCaps: !state.isAllCaps }));
+  const toggleAllCaps = () => setMemeDataTo(memeData => ({
+    ...memeData,
+    isAllCaps: !memeData.isAllCaps
+  }));
 
-  setPos = (elem, { top, left }) => {
+  const setPos = (elem, { top, left }) => {
     const { id } = elem;
-    this.setState(state => ({
-      pos: { ...state.pos, [id]: { top, left } }
+    setMemeDataTo(memeData => ({
+      ...memeData,
+      pos: {
+        ...memeData.pos,
+        [id]: { top, left }
+      }
     }));
   };
 
-  handleChange = ({ currentTarget: { name, value } }) => {
+  const handleChange = ({ currentTarget: { name, value } }) => {
     const number = Number.parseInt(value, 10); //if the value can be converted to a number type, convert it to a number type.
-    this.setState({ [name]: !isNaN(number) ? number : value });
+    setMemeDataTo(memeData => ({
+      ...memeData,
+      [name]: !isNaN(number) ? number : value
+    }));
   };
 
-  handleClick = ({ currentTarget: { name } }) => {
-    const { props, setEditModeTo } = this;
+  const handleClick = ({ currentTarget: { name } }) => {
     const { deleteMeme, id } = props;
 
     if (name === 'deleteBtn') deleteMeme(id);
     else if (name === 'editBtn') setEditModeTo(true);
   };
 
-  render = () => {
-    const { props, state, resetState, setTexts, toggleAllCaps, setEditModeTo, setPos, handleChange, handleClick } = this;
-    const { id, imgsData, saveMemeEdits } = props;
-    const { editModeOn, imgIdx, texts, fontIdx, fontSize, isAllCaps, pos } = state;
+  const { id, imgsData, saveMemeEdits } = props;
+  const { editModeOn, imgIdx, texts, fontIdx, fontSize, isAllCaps, pos } = memeData;
 
-    const memeImgProps = { imgsData, imgIdx: props.imgIdx, texts: props.texts, fontIdx: props.fontIdx, fontSize: props.fontSize, isAllCaps: props.isAllCaps, pos: props.pos };
+  const memeImgProps = { imgsData, imgIdx: props.imgIdx, texts: props.texts, fontIdx: props.fontIdx, fontSize: props.fontSize, isAllCaps: props.isAllCaps, pos: props.pos };
 
-    const editMemeImgProps = { imgsData, imgIdx, texts, fontIdx, fontSize, isAllCaps, pos, setPos };
+  const editMemeImgProps = { imgsData, imgIdx, texts, fontIdx, fontSize, isAllCaps, pos, setPos };
 
-    const editMemeFormProps = { id, imgsData, saveMemeEdits, imgIdx, texts, fontIdx, fontSize, isAllCaps, pos, resetState, setTexts, toggleAllCaps, setEditModeTo, handleChange };
+  const editMemeFormProps = { id, imgsData, saveMemeEdits, imgIdx, texts, fontIdx, fontSize, isAllCaps, pos, resetState, setTexts, toggleAllCaps, setEditModeTo, handleChange };
 
-    return (
-      <div className='MemeItem'>
-        {!editModeOn &&
-          <>
-            <MemeImg {...memeImgProps} />
-            <button name='editBtn' type='button'
-              onClick={handleClick}>Edit</button>
-            <button name='deleteBtn' type='button'
-              onClick={handleClick}>
-              <FontAwesomeIcon icon={faTrashAlt} />
-            </button>
-          </>}
-        {editModeOn &&
-          <>
-            <EditMemeImg {...editMemeImgProps} />
-            <EditMemeForm {...editMemeFormProps} />
-          </>
-        }
-      </div>
-    );
-  }
+  return (
+    <div className='MemeItem'>
+      {!editModeOn &&
+        <>
+          <MemeImg {...memeImgProps} />
+          <button name='editBtn' type='button'
+            onClick={handleClick}>Edit</button>
+          <button name='deleteBtn' type='button'
+            onClick={handleClick}>
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </button>
+        </>}
+      {editModeOn &&
+        <>
+          <EditMemeImg {...editMemeImgProps} />
+          <EditMemeForm {...editMemeFormProps} />
+        </>
+      }
+    </div>
+  );
 };
+
+export default MemeItem;
